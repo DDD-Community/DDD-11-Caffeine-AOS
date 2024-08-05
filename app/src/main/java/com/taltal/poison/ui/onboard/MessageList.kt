740ca78a89241ce4d9e00448668149bf2ecb846b
koggily <dingyujin@naver.com>
2024-08-05 21:13:51 +0900

    Poe 아이콘 및 메시지 리스트 추가

---
+++ app/src/main/java/com/taltal/poison/ui/onboard/MessageList.kt
@@ -0,0 +1,10 @@
+package com.taltal.poison.ui.onboard
+
+val messageList = listOf(
+    "닉네임을\n입력해주세요",
+    "포이즌을 사용하는\n목적을 선택해주세요",
+    "포이즌을 사용하는\n목적을 알려주세요",
+    "기본정보 바탕으로\n추천 목표를\n설정하고 있어요",
+    "목표를 선택해주세요"
+)
+
