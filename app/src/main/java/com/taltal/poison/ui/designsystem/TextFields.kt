75f8afbae0581bb00a25fbe41c7a754037bcad14
koggily <dingyujin@naver.com>
2024-08-05 21:09:18 +0900

    RoundedTextField 개발

--- app/src/main/java/com/taltal/poison/ui/designsystem/TextFields.kt
+++ app/src/main/java/com/taltal/poison/ui/designsystem/TextFields.kt
@@ -6,12 +6,23 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,10 +31,90 @@ import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.taltal.poison.ui.theme.body_16rg
 import com.taltal.poison.ui.theme.dialogue_14rg
+import com.taltal.poison.ui.theme.taltal_neutral_10
+import com.taltal.poison.ui.theme.taltal_neutral_50
+import com.taltal.poison.ui.theme.taltal_neutral_60
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RoundedTextField(
+    textHint: String = "",
+    onValueChanged: (String) -> Unit = {},
+    checkError: (String) -> Boolean = { false },
+    needClearButton: Boolean = false,
+    maxLine: Int = 1,
+    modifier: Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text
+) {
+    var textInput by remember { mutableStateOf("") }
+    var isError by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val scope = rememberCoroutineScope()
+
+    Column(modifier = modifier) {
+        TextField(
+            value = textInput,
+            onValueChange = {
+                onValueChanged(it)
+                textInput = it
+                isError = checkError(it)
+            },
+            placeholder = {
+                Text(
+                    text = textHint,
+                    style = body_16rg,
+                    color = taltal_neutral_50
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    1.dp,
+                    if (isError) Color.Red else taltal_neutral_10,
+                    RoundedCornerShape(8.dp)
+                ),
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                containerColor = Color.White
+            ),
+            trailingIcon = {
+                if (needClearButton && textInput.isNotEmpty()) {
+                    IconButton(onClick = {
+                        onValueChanged("")
+                        textInput = ""
+                        isError = false
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Clear,
+                            contentDescription = "Clear input",
+                            tint = taltal_neutral_60
+                        )
+                    }
+                }
+            },
+            maxLines = maxLine,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType,imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    scope.launch {
+                        keyboardController?.hide()
+                    }
+                }
+            )
+        )
+    }
+}
 
 @Composable
 fun CharacterMessage(text: String) {
