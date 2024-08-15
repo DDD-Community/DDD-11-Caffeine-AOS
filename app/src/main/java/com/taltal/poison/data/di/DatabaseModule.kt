2a6c841daf3b0cd76c369fd604e92ba7ea6a3fe3
koggily <dingyujin@naver.com>
2024-08-15 17:55:37 +0900

    DB 관련 DI 파일추가

---
+++ app/src/main/java/com/taltal/poison/data/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.taltal.poison.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.taltal.poison.data.AppDatabase
+import com.taltal.poison.data.api.CoffeeLogDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "poison_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideCoffeeIntakeDao(database: AppDatabase): CoffeeLogDao {
+        return database.coffeeLogDao()
+    }
+}
