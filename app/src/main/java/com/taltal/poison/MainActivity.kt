6de31223813549e01d1178ca417e674def833cf1
koggily <dingyujin@naver.com>
2024-08-28 23:42:46 +0900

    홈 화면에 네비게이션 적용 및 린트 적용

--- app/src/main/java/com/taltal/poison/MainActivity.kt
+++ app/src/main/java/com/taltal/poison/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.taltal.poison.ui.navigation.PoisonRootRoute
 import com.taltal.poison.ui.theme.PoisonTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,27 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PoisonTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PoisonRootRoute()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("Android")
-}
