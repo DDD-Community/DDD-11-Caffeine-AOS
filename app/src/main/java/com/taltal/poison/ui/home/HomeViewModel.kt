708574fda85eda877affc8f7553d816cc4996a5c
sezikim <kdghj1002@gmail.com>
2024-09-01 18:47:33 +0900

    API 연동 전 작업 끝

--- app/src/main/java/com/taltal/poison/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/taltal/poison/ui/home/HomeViewModel.kt
@@ -1,14 +1,57 @@
 package com.taltal.poison.ui.home
 
 import androidx.lifecycle.ViewModel
-import com.taltal.poison.data.api.PoisonApi
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
-    poisonApi: PoisonApi
-) : ViewModel() {
+class HomeViewModel @Inject constructor() : ViewModel() {
+    val uiState = MutableStateFlow(
+        HomeUiState(
+            currentPoison = 1,
+            purposePoison = 4,
+            description = "카페인 과다섭취는 두통, 불면증, 행동 불안,\n혈압 상승 등을 일으킬 수 있대요!",
+            imageUrl = ""
+        )
+    )
 
+    init {
+        getPoisonState(false)
+    }
 
+    fun getPoisonState(isDummyAddCall: Boolean) {
+        if (isDummyAddCall) {
+            uiState.value = uiState.value.copy(currentPoison = uiState.value.currentPoison + 1)
+            return
+        }
+        // TODO API CALL
+        viewModelScope.launch {
+            // uiState.value = fetchPoisonState()
+        }
+    }
+
+    fun drink(isDummyCall: Boolean) {
+        viewModelScope.launch {
+            addPoison()
+            getPoisonState(isDummyCall)
+        }
+    }
+
+    private suspend fun fetchPoisonState() {
+        // TODO API CALL
+    }
+
+    private suspend fun addPoison() {
+        // TODO API CALL
+    }
 }
+
+data class HomeUiState(
+    val currentPoison: Int,
+    val purposePoison: Int,
+    val description: String,
+    val imageUrl: String,
+)
